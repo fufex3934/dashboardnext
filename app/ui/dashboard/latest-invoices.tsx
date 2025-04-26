fcc7ded4516accd15105d1c218670998bc75b5ad
fufex3934 <fufawakgari174@gmail.com>
2025-04-26 17:26:01 +0300

    adding search and pagination

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -27,7 +27,7 @@ export default async function LatestInvoices() {
                 )}
               >
                 <div className="flex items-center">
-                  <Image
+                  <Image 
                     src={invoice.image_url || '/avatar.jpg'}
                     alt={`${invoice.name}'s profile picture`}
                     className="mr-4 rounded-full"
